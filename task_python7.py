776d5c38cb25317d06a042348ee18ba7a93eedcb
Kiryll <orlovich090789@list.ru>
2024-02-01 21:11:41 +0300

    home_work

--- task_python7.py
+++ task_python7.py
@@ -15,26 +15,26 @@
 
 # Напишите функцию, которая генерирует псевдоимена. Имя должно начинаться с заглавной буквы, состоять из 4-7 букв, среди которых обязательно должны быть гласные. Полученные имена сохраните в файл.
 
-from string import ascii_lowercase
+# from string import ascii_lowercase
 
-from random import choice, randint
+# from random import choice, randint
 
-VOWELS = 'a', 'o', 'e', 'i', 'u', 'y'
-LETTERS = ascii_lowercase
-
-
-def generate_pseudonyms(min_len=3, max_len=6) -> str:
-    name = choice(VOWELS).upper() + ''.join(choice(LETTERS) for _ in range(randint(min_len, max_len)))
-    return name
+# VOWELS = 'a', 'o', 'e', 'i', 'u', 'y'
+# LETTERS = ascii_lowercase
 
 
-def save_name_to_file(file_name: str, rows: int):
-    with open(file_name, 'w', encoding='utf-8') as f:
-        f.writelines('\n'.join(generate_pseudonyms() for _ in range(rows)))
+# def generate_pseudonyms(min_len=3, max_len=6) -> str:
+#     name = choice(VOWELS).upper() + ''.join(choice(LETTERS) for _ in range(randint(min_len, max_len)))
+#     return name
 
 
-if __name__ == '__main__':
-    save_name_to_file('names.txt', 5)
+# def save_name_to_file(file_name: str, rows: int):
+#     with open(file_name, 'w', encoding='utf-8') as f:
+#         f.writelines('\n'.join(generate_pseudonyms() for _ in range(rows)))
+
+
+# if __name__ == '__main__':
+#     save_name_to_file('names.txt', 5)
 
 #Напишите функцию, которая открывает на чтение созданные в прошлых задачах файлы с числами и именами. Перемножьте пары чисел. В новый файл сохраните имя и произведение:
 # если результат умножения отрицательный, сохраните имя записанное строчными буквами и произведение по модулю
@@ -122,3 +122,79 @@ if __name__ == '__main__':
 
 # if __name__ == '__main__':
 #     create_files_ext(".pdf", num_files=3)
+
+
+# # Напишите функцию группового переименования файлов в папке test_folder под названием rename_files. Она должна:
+# a. принимать параметр желаемое конечное имя файлов. При переименовании в конце имени добавляется порядковый номер.
+# b. принимать параметр количество цифр в порядковом номере.
+# c. принимать параметр расширение исходного файла. Переименование должно работать только для этих файлов внутри каталога.
+# d. принимать параметр расширение конечного файла.
+# e. принимать диапазон сохраняемого оригинального имени. Например для диапазона [3, 6] берутся буквы с 3 по 6 из исходного имени файла. К ним прибавляется желаемое конечное имя, если оно передано. Далее счётчик файлов и расширение.
+# f. Папка test_folder доступна из текущей директории
+
+# import os
+
+
+# def rename_files(desired_name, num_digits, source_ext, target_ext, name_range=None):
+#     new_names = []
+#     # Получаем список файлов в текущей директории
+#     files = os.listdir('test_folder')
+#     # Фильтруем только нужные файлы с указанным расширением
+#     filtered_files = [file for file in files if file.endswith(source_ext)]
+#     # Сортируем файлы по имени
+#     filtered_files.sort()
+#     # Задаем начальный номер для порядкового номера
+#     num = 1
+
+#     # Переименовываем файлы
+#     for file in filtered_files:
+#         # Получаем имя файла без расширения
+#         name = os.path.splitext(file)[0]
+#         # Если задан диапазон, обрезаем имя файла
+#         if name_range:
+#             name = name[name_range[0]-1:name_range[1]]
+        
+#         # Создаем новое имя с желаемым именем, порядковым номером и новым расширением
+#         new_name = desired_name + str(num)/zfill(num_digits) + '.' + target_ext
+
+#         # Переименовываем файл
+#         path_old = os.path.join(os.getcwd(), folder_name, file)
+#         path_new = os.path.join(os.getcwd(), folder_name, new_name)
+#         os.rename(path_old, path_new)
+
+#         # Увеличиваем порядковый номер
+#         num += 1
+# Введите ваше решение ниже
+code_to_write = '''
+import os
+
+def rename_files(desired_name, num_digits, source_ext, target_ext, name_range=None):
+    new_names = []
+
+    files = os.listdir('geekbrains')
+
+    filtered_files = [file for file in files if file.endswith(source_ext)]
+
+    filtered_files.sort()
+
+    num = 1
+
+    for file in filtered_files:
+        name = os.path.splitext(file)[0]
+
+        if name_range:
+            name = name[name_range[0]-1:name_range[1]]
+
+        new_name = desired_name + str(num).zfill(num_digits) + '.' + target_ext
+
+        os.rename(file, new_name)
+
+        new_names.append(new_name)
+
+        num += 1
+
+    return new_names
+'''
+
+with open("__init__.py", "w") as init_file:
+    init_file.write(code_to_write)
