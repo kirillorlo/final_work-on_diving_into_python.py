083bff9138263b7830cb7ea418a82c0064b09bd4
Kiryll <orlovich090789@list.ru>
2024-02-23 01:47:49 +0300

    home_work

--- testing/task_test.py
+++ testing/task_test.py
@@ -1,6 +1,18 @@
 # Создайте функцию, которая удаляет из текста все символы кроме букв латинского алфавита и пробелов. Возвращается строка в нижнем регистре.
 
-#Напишите для задачи 1 тесты doctest. Проверьте следующие варианты:
+# from string import ascii_letters
+
+
+# def clear_text(s: str) -> str:
+#     chars = ascii_letters + ' '
+#     return ''.join([c.lower() for c in s if c in chars])
+
+
+# if __name__ == '__main__':
+#     print(clear_text('Hello world'))
+
+
+# Напишите для задачи 1 тесты doctest. Проверьте следующие варианты:
 # возврат строки без изменений
 # возврат строки с преобразованием регистра без потери символов
 # возврат строки с удалением знаков пунктуации
@@ -11,9 +23,23 @@ from string import ascii_letters
 
 
 def clear_text(s: str) -> str:
+    """
+    >>> clear_text('hello world')
+    'hello world'
+    >>> clear_text('Hello world')
+    'hello world'
+    >>> clear_text('Hello world!')
+    'hello world'
+    >>> clear_text('Hello мир')
+    'hello '
+    >>> clear_text('Hello, world или мир!')
+    'hello world  '
+    """
     chars = ascii_letters + ' '
-    return ''.join([c.lower() for c in s if c in chars])
+    return ''.join([c for c in s if c in chars]).lower()
 
 
 if __name__ == '__main__':
-    print(clear_text('Hello world'))
+    import doctest
+
+    doctest.testmod(verbose=True)
